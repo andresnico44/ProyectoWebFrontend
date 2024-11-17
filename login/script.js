7d86ce051dd1bbc8896e482a0a545c4f4dfe72ec
andresnico44 <ramiroandresnc@ufps.edu.co>
2024-11-17 17:24:28 -0500

    Ajustes login

--- login/script.js
+++ login/script.js
@@ -4,7 +4,7 @@ const validCredentials = {
 };
 
 function authenticateUser(event) {
-    event.preventDefault();  // Evita el envío del formulario de forma predeterminada
+    event.preventDefault();
     const form = document.getElementById("login-form");
 
     if (form.checkValidity()) {
@@ -12,13 +12,11 @@ function authenticateUser(event) {
         const password = document.getElementById("password").value.trim();
 
         if (code === validCredentials.code && password === validCredentials.password) {
-            alert("¡Bienvenido!");
             window.location.href = "../principal/index.html";
         } else {
             alert("Código o contraseña incorrectos. Por favor, intente nuevamente.");
         }
     } else {
-        // Permitir que el navegador muestre los mensajes de los campos requeridos
         form.reportValidity();
     }
 }
